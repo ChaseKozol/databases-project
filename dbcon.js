fb14d4e5b92397828e5efd5a1cece323f8461c2c
Chase <chasekozol@yahoo.com>
2020-03-02 15:48:05 -0800

    Epstein didnt

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_kozolc',
-  password        : '',
+  password        : 'Djr3267cc7623rjD',
   database        : 'cs340_kozolc'
 });
 module.exports.pool = pool;
