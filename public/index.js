5bee71cd6375a0f0f26769d5b2044b5cb2ad3410
Chase Kozol <chasekozol@yahoo.com>
2020-03-06 21:57:14 -0800

    Fixed additem form styling

--- public/index.js
+++ public/index.js
@@ -2,12 +2,12 @@ var addItemButton = document.getElementById('add-item-button');
 addItemButton.addEventListener('click', unhide);
 
 function unhide() {
-	document.getElementById('add-item-form').style.display = "block";
+	document.getElementById('form-box').style.display = "block";
 }
 
 var cancelButton = document.getElementById('form-cancel');
 cancelButton.addEventListener('click', hide);
 
 function hide() {
-	document.getElementById('add-item-form').style.display = "none";
+	document.getElementById('form-box').style.display = "none";
 }
