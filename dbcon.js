2fa6c78d39fb95c5be4cea1b073ba0f03a8704bf
AlexanderSmith2098 <43623970+AlexanderSmith2098@users.noreply.github.com>
2020-03-09 19:05:51 -0700

    fixed error

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit     : 10,
   host                : 'classmysql.engr.oregonstate.edu',
-  user                : 'cs340_smitale2',
-  password            : '2829',
-  database            : 'cs340_smitale2',
+  user                : 'cs340_username',
+  password            : '',
+  database            : 'cs340_username',
   multipleStatements  : true
 });
 module.exports.pool = pool;
